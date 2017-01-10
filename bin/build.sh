b43fa156e69ed9b4a8acb9fff85de19f138d3523
Joe Grund <joe.grund@intel.com>
2017-01-10 11:08:38 -0500

    remove more stuff from build
    Signed-off-by: Joe Grund <joe.grund@intel.com>

--- bin/build.sh
+++ bin/build.sh
@@ -2,14 +2,7 @@
 
 rm -rf dist
 
-babel -d dist "test/*.js"
-
-if [[ $1 == "cover" ]]; then
-  # Instrumenting the src file.
-  babel -d dist "./source/fp.js" --plugins __coverage__
-else
-  babel -d dist "./source/fp.js"
-fi
+babel -d dist "./source/fp.js"
 
 for i in $( find ./source/*.flow -type f ); do
   cp $i ./dist/$i
