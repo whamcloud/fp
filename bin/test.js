72cc41c232b8d093b89b5a538e9f85ea89b6638e
Joe Grund <joe.grund@intel.com>
2017-04-25 11:55:11 -0400

    Update for open source (#2)
    * Update for open source
    
    * updates
    
    Signed-off-by: Joe Grund <grundjoseph@gmail.com>

--- bin/test.js
+++ bin/test.js
@@ -4,27 +4,11 @@
 
 var Jasmine = require('jasmine');
 var jasmine = new Jasmine();
-require('intel-jasmine-n-matchers');
-
-if (process.env.RUNNER === 'CI') {
-  var jasmineJUnitReporter = require('intel-jasmine-junit-reporter');
-
-  var junitReporter = jasmineJUnitReporter({
-    specTimer: new jasmine.jasmine.Timer(),
-    JUnitReportSavePath: process.env.SAVE_PATH || './',
-    JUnitReportFilePrefix: process.env.FILE_PREFIX || 'fp-results-' +  process.version,
-    JUnitReportSuiteName: 'FP Reports',
-    JUnitReportPackageName: 'FP Reports'
-  });
-
-  jasmine.jasmine.getEnv().addReporter(junitReporter);
-}
+require('@mfl/jasmine-n-matchers');
 
 jasmine.loadConfig({
   spec_dir: 'dist/test',
-  spec_files: [
-    '**/*-test.js'
-  ],
+  spec_files: ['**/*-test.js'],
   random: true
 });
 
